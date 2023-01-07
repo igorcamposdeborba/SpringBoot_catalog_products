86abb565304cc27299a2cd3b6ec75f8d903fa445
igorborba <igor_campos5@hotmail.com>
2023-01-07 14:26:25 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	
+	@Transactional(readOnly = true) // transação sempre executa esta operação no banco de dados. ReadOnly true não trava o banco (boa prática em operações de leitura)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
---
+++ query.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE testdb;
+
+CREATE USER 'sa'@'localhost' IDENTIFIED BY '';
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'sa'@'localhost' WITH GRANT OPTION;
+
+USE testdb;
+
+CREATE TABLE categories(
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256),
+	PRIMARY KEY(id)
+);
+DROP TABLE categories;
+
+SELECT * FROM categories;
+
+ALTER TABLE categories CHANGE id id INT AUTO_INCREMENT PRIMARY KEY NOT NULL;
+
+INSERT INTO categories (name) VALUES("Teste 1");
+INSERT INTO categories (name) VALUES("Teste 2");
+INSERT INTO categories (name) VALUES(null);
