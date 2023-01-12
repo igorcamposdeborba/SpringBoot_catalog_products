9d4be31f46a4a8acd428d57bceee939df9afd6a3
igorborba <igor_campos5@hotmail.com>
2023-01-11 22:36:45 -0300

    Product DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,92 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+
+public class ProductDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	private Instant date;
+
+	private List<CategoryDTO> categories = new ArrayList<>();
+	
+	public ProductDTO() {}
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+	}
+	
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity); // chamar construtor de cima
+		
+		for (Category i : categories) {
+			this.categories.add(new CategoryDTO(i));
+		}
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	public Set<Category> getCategories(){
+		return categories;
+	}
+
+}
