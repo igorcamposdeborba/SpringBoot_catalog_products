5d9af5103c11be4a480de29cebd2c96758362431
igorborba <igor_campos5@hotmail.com>
2023-02-02 11:26:16 -0300

    Implement find all paged test with JUnit

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTest.java
@@ -1,13 +1,21 @@
 package com.devsuperior.dscatalog.services;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 
 @SpringBootTest // Carrega o contexto da aplicação (teste de integração)
+@Transactional // Rollback no banco de dados depois de cada teste
 public class ProductServiceIntegrationTest {
 
 	@Autowired
@@ -39,6 +47,46 @@ public class ProductServiceIntegrationTest {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
-		
 	}
+	
+	@Test
+	public void findAllPagedShouldReturnPagedWhenPageZeroSizeTen() {
+		// Arrange
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		// Act
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		 
+		// Aseert
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(10, result.getNumberOfElements());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());	
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+	}
+	
+	
+	
 }
