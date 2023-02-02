7a7d261c1b9c8d9d5117a4ef89fea9bcaa16d30d
igorborba <igor_campos5@hotmail.com>
2023-02-02 10:24:49 -0300

    Add integration test for delete resource with JUnit

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+
+@SpringBootTest // Carrega o contexto da aplicação (teste de integração)
+public class ProductServiceIntegrationTest {
+
+	@Autowired
+	ProductService service;
+	
+	@Autowired
+	ProductRepository repository;
+	
+	private Long existingid;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		existingid = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+		}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		service.delete(existingid);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+	
+}
