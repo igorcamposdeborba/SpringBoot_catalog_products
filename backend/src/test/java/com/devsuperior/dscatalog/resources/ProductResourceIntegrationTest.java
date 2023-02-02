eaa567675bc8cbdfaae8de12b501d25f3ca82deb
igorborba <igor_campos5@hotmail.com>
2023-02-02 13:01:15 -0300

    Integration test update resource with JUnit

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIntegrationTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,6 +17,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest // Carrega o contexto da aplicação (teste de integração & web)
 @AutoConfigureMockMvc // Trata as requisições sem subir o servidor
 @Transactional // Rollback do banco após cada requisição
@@ -24,6 +29,9 @@ public class ProductResourceIntegrationTest {
 
 	@Autowired
 	private MockMvc mockMvc; // permite que eu faça requisições
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	private Long existingid;
 	private Long nonExistingId;
@@ -36,12 +44,13 @@ public class ProductResourceIntegrationTest {
 		countTotalProducts = 25L;
 	}
 	
+	// Teste de integração na camada web para find all paginado (paginação personalizada)
 	@Test
 	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
 		
 		ResultActions result = mockMvc.perform(get("/products?page=0&linesPerPage=100&direction=ASC&orderBy=name")
 													.accept(MediaType.APPLICATION_JSON));
-	
+		
 		result.andExpect(status().isOk());
 		result.andExpect(jsonPath("$.content").exists());
 		result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
@@ -51,7 +60,30 @@ public class ProductResourceIntegrationTest {
 		
 	}
 	
-	
+	public void updateShouldReturnProductDTOWhenExistsId() throws Exception {
+		ProductDTO productDTO = Factory.createProductDto();
+		
+		Long expectedId = productDTO.getId();
+		String expectedName = productDTO.getName();
+		String expectedDescription = productDTO.getDescription();
+		Double expectedPrice = productDTO.getPrice();
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO); // converter objeto java em String para o JSON
+		
+		ResultActions result = mockMvc.perform(put("/products/{id}", existingid)
+												.content(jsonBody)
+												.contentType(MediaType.APPLICATION_JSON)
+												.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(expectedId)); // testar se objeto que voltou na resposta do banco de dados tem o mesmo id instanciado
+		result.andExpect(jsonPath("$.name").value(expectedName));
+		result.andExpect(jsonPath("$.description").value(expectedDescription));
+		result.andExpect(jsonPath("$.price").value(expectedPrice));
+		
+		
+		
+	}
 	
 	
 	
