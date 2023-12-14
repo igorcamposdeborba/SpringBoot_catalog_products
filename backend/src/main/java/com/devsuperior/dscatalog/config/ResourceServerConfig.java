a4bfba257bc6b4ee7691dcbc7d3ae0074200c2a4
igorcamposdeborba <igor_campos5@hotmail.com>
2023-12-13 23:09:35 -0300

    Release of H2 database to access without TokenJWT

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,10 +17,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Autowired
+	private Environment environment; // ambiente de execução da aplicação para acessar H2
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	// Rotas (path da URL):
-	private static final String[] PUBLIC = {"/oauth/token"}; // rotas públicas para página de login, para não precisar de login para mostrar a página de login
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; // rotas públicas para página de login, para não precisar de login para mostrar a página de login
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"}; // rotas públicas sem precisar de login, para catálogo
 	private static final String[] ADMIN = {"/users/**"}; // rotas para admin logado
 	
@@ -28,6 +34,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		// liberar rota (url) do H2
+		if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 				.antMatchers(PUBLIC).permitAll() // define autorizações
 				.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() // liberar para todos não logados (eles não têm token)
